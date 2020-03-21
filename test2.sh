d38d255933ff7175c05669c3d98b846968ea1d4f
LuZhanXing <18330259691@qq.com>
2020-03-21 09:47:26 +0800

    3.21-9:47

--- test2.sh
+++ test2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
 
-echo "自动提交开始"
+echo "自动提交开始......"
 git add .
 #echo "git提交注释:$1"
 read -p "请输入commit提示信息：" commit_m
 git commit -m $commit_m
-git push origin master
-echo "自动提交完成"
+git push origin $(git rev-parse --abbrev-ref HEAD)
+#echo "自动提交完成"
+sleep 3s
