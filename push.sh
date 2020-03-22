683b27f44f309eb6656b0f1f4929c060099fe700
LuZhanXing <18330259691@qq.com>
2020-03-22 14:03:57 +0800

    3.22-14:03

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ git_branch()
   if [ "$branch_switch" == "Y" ];then
 #    git_name()
 #    {
-       read -p '请输入要切换的分支的名称（注意：1.请谨慎输入，如果输入有误的话将会提示“fatal：invalid reference”信息并且本程序将会退出，您需要重新开始！2.如果输入成功的话，将会提示“switched to branch ...”信息并且进入新的分支，您在本分支之内做的操作将不会上传到远程仓库）：
+       read -p '请输入要切换的分支的名称（注意：1.请谨慎输入，如果输入有误的话将会提示“fatal：invalid reference”信息并且本程序将会退出，您需要重新开始！ 2.如果输入成功的话，将会提示“switched to branch ...”信息并且进入新的分支，您在本分支之内做的操作将不会上传到远程仓库）：
        ' branch_name 
        git switch -f $branch_name
 #       if [[ "$branch_name" = "$(git branch)" ]];then
@@ -26,10 +26,10 @@ git_branch()
   echo '即将进行将本地工作空间的内容提交到远程仓库的一系列操作...'    
   elif [  "$branch_switch" == "N" ];then
        git add .
-       read -p '请输入提交描述信息(注意：1.描述信息不能为空，必须输入！！！2.如果您没有对工作空间的内容做任何操作，将会提示“”信息)：
+       read -p '请输入提交描述信息(注意：1.描述信息不能为空，必须输入！！！  2.如果您没有对工作空间的内容做任何操作，将会提示“nothing to commit, working tree clean ”信息)：
        ' commit_m
        git commit -m  $commit_m  
-       echo '推送到远程仓库(如您没有对工作空间的内容做任何操作，将会提示“everything up-to-date”信息)：'
+       echo '推送到远程仓库(注意：1.如果您没有对工作空间的内容做任何操作，将会提示“everything up-to-date”信息。2.如果您成功上传到远程仓库，会提示“”信息)：'
        git push origin $(git rev-parse --abbrev-ref HEAD)
 #      echo '您已成功推送至远程仓库，改页面将在三秒后关闭......'
       sleep 3s
